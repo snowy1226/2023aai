553c61c876b56b6616b85626f72e7b4a8b343525
snowy1226 <144758304+snowy1226@users.noreply.github.com>
2023-09-26 11:52:38 +0800

    Add files via upload

---
+++ week03-3.py
@@ -0,0 +1,4 @@
+for i in range(1,10):
+ for a in range(1,10):
+   print(i, 'x', a, '=',i*a, sep='', end=' ')
+ print()
---
+++ week03-4.py
@@ -0,0 +1,3 @@
+for i in range(1,10):
+  for a in range(1,10):
+    print(f'{i}x{a}={i*a}')
