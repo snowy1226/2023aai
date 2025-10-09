cac0a5a3a97505272e17930d1ff4519f4244a422
snowy1226 <144758304+snowy1226@users.noreply.github.com>
2025-10-09 17:11:27 +0800

    Add files via upload

---
+++ week05-1.cpp
@@ -0,0 +1,17 @@
+//week05-1.cpp
+//學習計畫 built-in functions 58.
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s); //把字串,變成之前教的cin iostream
+        string word; //字串的word
+        //ss >> word; //像之前教的 cin >> word
+        //cont << "讀到了" << word << "/n";
+        while( ss >> word ){ //一直讀近來
+            //裡面甚麼都不做
+        }
+        return word.length(); //最後的字的長度 //return 0; 
+
+        
+    }
+};
